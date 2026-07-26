70d22b0f5d4d52abf21837d147f0af2fe1970926
yui-Kitamura <yui@yui-kitamura.eng.pro>
2026-07-26 23:41:43 +0900

    update depend core ver to v2.0.0

--- src/main/java/pro/eng/yui/oss/osm/jppostaldata/worker/PrefectureDataJsonGenerator.java
+++ src/main/java/pro/eng/yui/oss/osm/jppostaldata/worker/PrefectureDataJsonGenerator.java
@@ -75,7 +75,7 @@ public class PrefectureDataJsonGenerator {
         Map<String, Object> data = new HashMap<>();
 
         String query = OverpassQuery.getPostSearchQuery(prefName);
-        List<OsmPoi> pois = JpPostalUtil.callOverpass(query, 5, 30, 120);
+        List<OsmPoi> pois = JpPostalUtil.callOverpass(query, 5, 30, 120).join();
 
         LocalDateTime timestamp = LocalDateTime.now(Main.JST);
         data.put("lastModified", timestamp.format(Main.FORMATTER));
